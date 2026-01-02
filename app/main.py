7e3fb78f20605fa04434b71d4d810e4ca6157cc4
Mohamed-kababi <mohamedkababi4@gmail.com>
2026-01-02 10:33:51 +0200

    add health endpoint for k8s probes

--- app/main.py
+++ app/main.py
@@ -8,6 +8,11 @@ from app.models import User
 app = FastAPI()
 
 
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+
 @app.get("/")
 def index(
     current_user: User = Depends(get_current_user),
